5f03963aa0a7fcbdc8f2a816f0eb243f96fa3510
Mohammed Farghally <mfseddik@opendsa.cs.vt.edu>
2015-11-03 12:21:03 -0500

    Finishing calculation

--- ScriptTimeInModule.php
+++ ScriptTimeInModule.php
@@ -41,49 +41,80 @@ for($i = 0;$i < count($data); $i++){
   }
 }
 
-print_r($result);
+//print_r($result);
 
 
-//calculate_time($result, $module_id, $book_id);
+//Finding module sessions for each user and then pass this to calculate_time to get the max
+$sessions = array();
+$session_start = false;
+$current_user = null;
+$module_load = null;
+for($i = 0;$i < count($result); $i++){
+  if($result[$i]['name'] == 'document-ready' && $session_start == false){
+    //Start of a session
+    $current_user = $result[$i]["user_id"];
+    $module_load = $result[$i];
+    $session_start = true;
+  }
+  else if ($result[$i]['name'] == 'document-ready' && $session_start == true){
+    //subsequent document-readys without matching window-unload
+    if($result[$i]["user_id"] != $current_user){
+      $current_user = $result[$i]["user_id"];
+      $module_load = $result[$i];
+    }
+  }
+  else if ($result[$i]['name'] == 'window-unload' && $session_start == true){
+    //window-unload matching a document ready
+    if($result[$i]['user_id'] == $current_user){
+      array_push($sessions, $module_load);  
+      array_push($sessions, $result[$i]);  
+    }
+    $session_start = false; 
+  }
+}
+
+print_r($sessions);
+
+calculate_time($sessions, $module_id, $book_id);
 
  
 //////////////////////////////////////////////////////////////////////////////////////////////
 
 function calculate_time($result, $module_id, $book_id){
   //Calculating the baseline data points
-  global $module_name;
   $maxDiff = 0;
   $seconds = array();
   for($i = 0;$i < count($result); $i += 2){
  
     $dateDiff = strtotime($result[$i+1]['action_time']) - strtotime($result[$i]['action_time']);
 	  //echo $dateDiff."     ";
-	  if($dateDiff > $maxDiff && $dateDiff <= 600){
+	  if($dateDiff > $maxDiff && $dateDiff <= 6000){
 	    $maxDiff = $dateDiff;
 	  }
 	  
     if($i < count($result)-2){
       if($result[$i+2]['user_id'] != $result[$i]['user_id']){
         //Add maxDiff to the list
-        array_push($seconds, array($result[$i]['user_id'],$maxDiff, $module_name[$module_id], $book_id));
+        array_push($seconds, array($result[$i]['user_id'],$maxDiff, $module_id, $book_id));
         $maxDiff = 0;
       }
     }
     else{
       //Adding the last one
-      array_push($seconds, array($result[$i]['user_id'],$maxDiff, $module_name[$module_id], $book_id));
+      array_push($seconds, array($result[$i]['user_id'],$maxDiff, $module_id, $book_id));
     
 	   }
     
   }
   print_r($seconds);
+  
   //Fill in csv file
-  $file = fopen("$module_name[$module_id] In Book $book_id Analysis.csv",'w');
-  //Adding headers
-  fputcsv($file, array("User_ID","Difference In Seconds" ,"Module Name","Book_ID"));
-  foreach($seconds as $line){
-    fputcsv($file, $line);
-  }
+  // $file = fopen("Module $module_id In Book $book_id Analysis.csv",'w');
+  // //Adding headers
+  // fputcsv($file, array("User_ID","Difference In Seconds" ,"Module Name","Book_ID"));
+  // foreach($seconds as $line){
+  //   fputcsv($file, $line);
+  // }
 
-  echo 'Data Written to file Successfuly\n';
+  // echo "Data Written to file Successfuly\n";
 }
